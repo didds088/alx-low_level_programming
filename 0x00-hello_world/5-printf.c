dd19bc14cb8fb8a6c9a11353575535f781f95d66
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-08 02:03:30 -0700

    Task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* Return: 0  */
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ *
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
