611f2cf576b30db094da9cea7b409396cfa04b1b
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-09 08:12:25 -0700

    Task 01

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,8 @@
 /**
  * main - Entry point
  *
- * Description: print the value of n status greater than, is zero and is not less than 6.
+ * Description: print the value of n status
+ * greater than, is zero and is not less than 6.
  *
  * Return: Always O (Success)
  */
