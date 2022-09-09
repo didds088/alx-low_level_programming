d4620ebdf70d3cf09039763a7e4222763d2fe759
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-09 08:03:00 -0700

    Task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-/*
+
+/**
  * main - Entry point
  *
- * Description: Get a random number and check its last digit, compare it with 5
+ * Description: print the value of n status:
+ * 		greater than, is zero and is not less than 6.
  *
- * Return: Always 0 (Success)
+ * Return: Always O (Success)
  */
 
 int main(void)
