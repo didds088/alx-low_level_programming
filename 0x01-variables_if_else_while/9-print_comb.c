f2b07a555bef418dd2150ac798eed66717191dda
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-09 09:55:19 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n + 48);
+
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
