73b33878a6ba4544974bbd54338a62f68218569f
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-13 01:28:14 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar, followed by a new line
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char n[] = "_putchar";
+
+	while (i <= 8)
+	{
+		_putchar(n[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
