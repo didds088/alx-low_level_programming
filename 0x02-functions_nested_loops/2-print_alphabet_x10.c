f824567202e4023ffd1f4efe16eaca15895c45df
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-13 02:54:59 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet x10
+ *
+ * Return (void)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	  while (i < 10)
+	  {
+		  char n = 'a';
+
+		  while (n <= 'z')
+		  {
+			  _putchar(n);
+			  n++;
+		  }
+		  _putchar('\n');
+		  i++;
+	  }
+}
