f10fdab042107cc926f37bc9920a057d87ff4778
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-13 09:14:57 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (n = 0; n <= 9; n++)
+	for (num = 0; num <= 9; ++num)
 	{
-		_putchar('0');
+		_putchar(48);
 		for (mult = 1; mult <= 9; ++mult)
 		{
 			_putchar(',');
@@ -21,13 +21,10 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
-			{
 				_putchar(' ');
-			}
 			else
-			{
 				_putchar((prod / 10) + 48);
-			}
+
 			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
