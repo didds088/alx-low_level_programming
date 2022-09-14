213cf0d05daafe68794c13fec3a7d4585f1617dc
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-14 00:26:59 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ *
+ * return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 90 % 2;
+	positive_or_negative(i);
+
+	return (0);
+}
