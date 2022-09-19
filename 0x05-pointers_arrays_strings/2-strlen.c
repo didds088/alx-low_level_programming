beee5e923a73039040a861a126224cdd5e751bdc
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-19 00:58:54 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - find the length of a string
+ * @s: pointer to the string to check
+ * Return: length of input
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+
+	return (len);
+}
