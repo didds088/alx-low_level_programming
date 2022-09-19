d7cd514d43c7a7c415a92285da197884773e756f
Agusiobo Kingsley <agusiobokingsley@gmail.com>
2022-09-19 05:03:47 -0700

    Task 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,13 +7,29 @@
  * generates random valid passwords for the program
  * Return passwords
  */
-
 int main(void)
 {
-	int num;
+	int pass[100];
+	int i, sum, n;
 
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
+	sum = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	putchar('\n');
 	return (0);
 }
